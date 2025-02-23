71bf73f60dd7c8a56671a0888a91c121c619d76a
Diogo Gomes <diogogomescastro@outlook.com>
2025-02-23 14:20:00 -0300

    fix: Image in AlunoCard

--- carometro-turbinado/src/components/AlunoCard.tsx
+++ carometro-turbinado/src/components/AlunoCard.tsx
@@ -5,21 +5,23 @@ import Image from 'next/image'
 export default function AlunoCard({ aluno }: { aluno: Aluno }) {
     const router = useRouter()
 
+    console.log("ALUNO1:", aluno);
+
     function navegarPerfilAluno(idTurma: string) {
         router.push(`/perfil/perfilAluno?id=${idTurma}`)
     }
 
-    return(
+    return (
         <button className="shadow-sm border-gray-900 border-1 bg-gray-50 rounded-lg w-96 h-32 p-3 flex hover:w-[25rem] transition-all cursor-pointer" onClick={() => navegarPerfilAluno(aluno.id)} key={aluno.id}>
-                <div className="border-gray-900 border-1 bg-white h-full w-20 flex items-center justify-center rounded-lg">
-                    <img src={aluno.fotoUrl} width={100} alt={`Foto do aluno ${aluno.nome}`}></img>
+            <div className="border-gray-900 border-1 bg-white h-full w-20 flex items-center justify-center rounded-lg">
+                <img src={aluno.fotoUrl} width={150} className="rounded-lg shadow-sm" alt={`Foto do Aluno ${aluno.nome}`} />
+            </div>
+            <div className="px-3 flex flex-col gap-3 text-start h-full py-1">
+                <p className="text-xl">{aluno.nome}</p>
+                <div className="">
+                    <p>{aluno.dataNascimento}</p>
                 </div>
-                <div className="px-3 flex flex-col gap-3 text-start h-full py-1">
-                    <p className="text-xl">{aluno.nome}</p>
-                    <div className="">
-                        <p>{aluno.dataNascimento}</p>
-                    </div>
-                </div>
+            </div>
         </button>
     )
 }
