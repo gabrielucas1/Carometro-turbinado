fdef3bd674e679935da16c5a29f3c8b22978388f
Diogo Gomes <diogogomescastro@outlook.com>
2025-03-31 00:41:45 -0300

    fix: Add .env example.env in project

--- carometro-turbinado/src/firebase/firebase.ts
+++ carometro-turbinado/src/firebase/firebase.ts
@@ -1,29 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import dotenv from "dotenv";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Load environment variables
+dotenv.config();
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVzFhxPFjp-Y1yfuLWZ9O9uboOdP3mgbQ",
-  authDomain: "carometro-turbinado.firebaseapp.com",
-  projectId: "carometro-turbinado",
-  storageBucket: "carometro-turbinado.appspot.com",
-  messagingSenderId: "630462104781",
-  appId: "1:630462104781:web:36716ab614e6971ee05584",
-  measurementId: "G-66S9LGMRXP"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 let analytics = null;
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   try {
     analytics = getAnalytics(app);
   } catch (error) {
@@ -39,5 +40,4 @@ const storage = getStorage(app);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-export {auth, provider, db, storage}
-
+export { auth, db, provider, storage };
